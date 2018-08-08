925eb5c1bbcc30078706dedfc63ede3f552795ea
teerus <z83@foxmail.com>
2018-08-08 17:10:45 +0800

    javaConfig显式装配

--- SpringTest/Chapter_02/src/main/java/soundsystem/CDPlayerConfig.java
+++ SpringTest/Chapter_02/src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,18 @@
 package soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = {CDPlayer.class})
 public class CDPlayerConfig {
+    @Bean
+    public CompactDisc sgtPeppers(){
+        return new SgtPeppers();
+    }
+    @Bean
+    public CDPlayer cdPlayer(CompactDisc compactDisc) {
+        return new CDPlayer(compactDisc);
+    }
+
 }
