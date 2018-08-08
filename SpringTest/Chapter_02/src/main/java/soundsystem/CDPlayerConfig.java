ff3e5f26f0cd4ff7315f83544d9e09f9f05c1e30
teerus <z83@foxmail.com>
2018-08-08 16:26:09 +0800

    设置文件扫描基础包

--- SpringTest/Chapter_02/src/main/java/soundsystem/CDPlayerConfig.java
+++ SpringTest/Chapter_02/src/main/java/soundsystem/CDPlayerConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackageClasses = {CDPlayer.class})
 public class CDPlayerConfig {
 }
--- SpringTest/Chapter_02/src/main/java/soundsystem/SgtPeppers.java
+++ SpringTest/Chapter_02/src/main/java/soundsystem/SgtPeppers.java
@@ -2,7 +2,7 @@ package soundsystem;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("lonelyHeartsClub")
 public class SgtPeppers implements CompactDisc {
 
     private String title = "Sgt. Pepper's Lonely Hearts Club Band";
