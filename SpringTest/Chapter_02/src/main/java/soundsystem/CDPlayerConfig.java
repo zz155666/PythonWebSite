ef08cd1f89e4760ce8fe53557706181d350e43fe
teerus <z83@foxmail.com>
2018-08-09 15:46:31 +0800

    混合装配

--- SpringTest/Chapter_02/src/main/java/soundsystem/CDPlayerConfig.java
+++ SpringTest/Chapter_02/src/main/java/soundsystem/CDPlayerConfig.java
@@ -3,13 +3,11 @@ package soundsystem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(CDConfig.class)
 public class CDPlayerConfig {
-    @Bean
-    public CompactDisc sgtPeppers(){
-        return new SgtPeppers();
-    }
     @Bean
     public CDPlayer cdPlayer(CompactDisc compactDisc) {
         return new CDPlayer(compactDisc);
