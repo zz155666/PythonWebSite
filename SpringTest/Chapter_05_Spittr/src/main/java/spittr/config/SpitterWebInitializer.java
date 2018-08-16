08b04499fa2eaa7364bd6f5b5c5e1f152aa18d3d
teerus <z83@foxmail.com>
2018-08-16 16:19:39 +0800

    web项目构建

--- SpringTest/Chapter_05_Spittr/src/main/java/spittr/config/SpitterWebInitializer.java
+++ SpringTest/Chapter_05_Spittr/src/main/java/spittr/config/SpitterWebInitializer.java
@@ -1,6 +1,7 @@
 package spittr.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import spittr.web.WebConfig;
 
 public class SpitterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
