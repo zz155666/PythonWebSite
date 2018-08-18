adfcb20d20e111107605b5b2bb29075c38411704
teerus <z83@foxmail.com>
2018-08-18 14:27:52 +0800

    使用注解执行

--- newtest/spring1/src/main/java/org/controller/Hello2Controller.java
+++ newtest/spring1/src/main/java/org/controller/Hello2Controller.java
@@ -1,5 +1,9 @@
 package org.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @ProjectName: spring1
  * @Package: org.controller
@@ -10,5 +14,19 @@ package org.controller;
  * @UpdateRemark: 更新说明
  * @Version: 1.0
  */
+
+@Controller
 public class Hello2Controller {
+    @RequestMapping(value = "/hello2")
+    public ModelAndView hello(){
+        System.out.println("hello2 方法被调用");
+        // 创建准备返回的ModelAndView对象，该对象通常包含了返回视图的路径、模型的名称以及模型对象
+        ModelAndView mv = new ModelAndView();
+        // 添加模型数据 可以是任意的POJO对象
+        mv.addObject("message", "Hello World 2!");
+        // 设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        mv.setViewName("/WEB-INF/content/welcome.jsp");
+        // 返回ModelAndView对象。
+        return mv;
+    }
 }
