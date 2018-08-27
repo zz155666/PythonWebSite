80d4b560f84fe5a517d219e12d12b7818246af25
teerus <z83@foxmail.com>
2018-08-27 17:36:29 +0800

    json解析

---
+++ newtest/spring2/src/main/java/org/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String helloWorld(Model model){
+        model.addAttribute("message","Hello World!");
+        return "index";
+    }
+}
