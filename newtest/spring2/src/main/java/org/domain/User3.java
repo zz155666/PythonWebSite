6d432814a36e60544221befd94c48eec1e34ce52
teerus <z83@foxmail.com>
2018-09-02 21:49:42 +0800

    测试checkbox标签

---
+++ newtest/spring2/src/main/java/org/domain/User3.java
@@ -0,0 +1,28 @@
+package org.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @ProjectName: spring2
+ * @Package: org.domain
+ * @ClassName: User3
+ * @Description: java类作用描述
+ * @Author: teerus
+ * @CreateDate: 2018/9/2 21:43
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class User3 implements Serializable {
+
+    private List<String> cources;
+    public User3(){
+        super();
+    }
+    public List<String> getCources(){
+        return cources;
+    }
+    public void setCources(List<String> cources){
+        this.cources=cources;
+    }
+}
