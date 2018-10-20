bea13d15dd5393170535a84e510cd81f200f0a6c
teerus <z83@foxmail.com>
2018-10-20 16:28:50 +0800

    spring装配

--- SpringInAction/Chapter_02/stereo-xmlconfig/src/main/java/soundsystem/CDPlayerConfig.java
+++ SpringInAction/Chapter_02/stereo-xmlconfig/src/main/java/soundsystem/CDPlayerConfig.java
@@ -4,17 +4,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CDPlayerConfig {
-
-  //生成接口对应的实例(CD接口生成一个光盘)
+  
   @Bean
   public CompactDisc compactDisc() {
     return new SgtPeppers();
   }
-
-  //将光盘插入CD播放器
+  
   @Bean
-  public CDPlayer cdPlayer(CompactDisc compactDisc) {
-    return new CDPlayer(compactDisc);
+  public CDPlayer cdPlayer() {
+    return new CDPlayer(compactDisc());
   }
 
 }
